acbf3044922a05b0a15eb477e933456008e0adcf
Vedant <122482472+vedantparmar12@users.noreply.github.com>
2024-06-12 15:14:44 +0530

    folder sturcture update

---
+++ templete.py
@@ -0,0 +1,37 @@
+import os
+from pathlib import Path
+import logging
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+project_name= "cnnClassifier"
+list_of_files=[
+    "github/workflows/.gitkeep",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.Py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml" "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb"
+
+]
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir,filename = os.path.split(filepath)
+    '''Spliting folder and file'''
+
+    if filedir != "":
+        os.makedirs(filedir,exist_ok=True) # exist_ok =True means if it exist it will not replaced
+        logging.info(f"Creating folder {filedir} for the file {filename}")
+    if(not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath,"w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+    else:
+        logging.info(f"File {filepath} already exists")
